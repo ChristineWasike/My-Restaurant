d2f2fd4066c9836c8a014ee33c741d0c77bd7253
Christine Wasike <christinewasike22@gmail.com>
2017-09-06 08:01:00 +0300

    Carried out a test on the code in the Restaurnat.java file

--- app/src/test/java/com/example/wasike/myrestaurants/RestaurantsActivityTest.java
+++ app/src/test/java/com/example/wasike/myrestaurants/RestaurantsActivityTest.java
@@ -1,8 +1,41 @@
 package com.example.wasike.myrestaurants;
 
+import android.os.Build;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNotNull;
+
+@Config(constants = BuildConfig.class, sdk = Build.VERSION_CODES.LOLLIPOP)
+@RunWith(RobolectricGradleTestRunner.class)
+
 /**
  * Created by wasike on 05/09/17.
  */
 
 public class RestaurantsActivityTest {
+    private RestaurantsActivity activity;
+    private ListView mRestaurantListView;
+
+    @Before
+    public void setup() {
+        activity = Robolectric.setupActivity(RestaurantsActivity.class);
+        mRestaurantListView = (ListView) activity.findViewById(R.id.listView);
+    }
+
+
+    @Test
+    public void restaurantsListViewPopulates() {
+        assertNotNull(mRestaurantListView.getAdapter());
+        assertEquals(mRestaurantListView.getAdapter().getCount(), 15);
+    }
+
+
 }
