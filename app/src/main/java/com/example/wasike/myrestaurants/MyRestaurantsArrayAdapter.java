961b3712bc407a6999cc7aa02246fdfe0de8fd4f
Christine Wasike <christinewasike22@gmail.com>
2017-09-06 08:32:20 +0300

    created to overriden methods getItem() and getCount() to display both cuisines and restaurants in the Restaurants activity

--- app/src/main/java/com/example/wasike/myrestaurants/MyRestaurantsArrayAdapter.java
+++ app/src/main/java/com/example/wasike/myrestaurants/MyRestaurantsArrayAdapter.java
@@ -1,8 +1,33 @@
 package com.example.wasike.myrestaurants;
 
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
 /**
  * Created by wasike on 06/09/17.
  */
 
-public class MyRestaurantsArrayAdapter {
+public class MyRestaurantsArrayAdapter extends ArrayAdapter {
+    private Context mContext;
+    private String[] mRestaurants;
+    private String[] mCuisines;
+
+    public MyRestaurantsArrayAdapter(Context mContext, int resource, String[] mRestaurants, String[] mCuisines) {
+        super(mContext, resource); //because I'm inheriting from a class with no default, parameterless constructor.
+        this.mContext = mContext;
+        this.mRestaurants = mRestaurants;
+        this.mCuisines = mCuisines;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        String restaurant = mRestaurants[position];
+        String cuisine = mCuisines[position];
+        return String.format("%s \nServes great: %s", restaurant, cuisine);
+    }
+
+    @Override
+    public int getCount(){
+        return mRestaurants.length; //required by the ListView to know how many items to draw into the UI.
+    }
 }
