3a253897506d1072109be9f8219b955abdd23c9a
Christine Wasike <christinewasike22@gmail.com>
2017-09-11 09:21:06 +0300

    Created a new Constants.java file used to call my token from my gradle.properties file with the name given

---
+++ app/src/main/java/com/example/wasike/myrestaurants/Constants.java
@@ -0,0 +1,10 @@
+package com.example.wasike.myrestaurants;
+
+/**
+ * Created by wasike on 11/09/17.
+ */
+
+public class Constants {
+    public static final String YELP_TOKEN = BuildConfig.YELP_TOKEN;
+
+}
