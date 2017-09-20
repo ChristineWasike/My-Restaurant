2a4b51df45dde21e6cef5bde3164125cc89844a2
Christine Wasike <christinewasike22@gmail.com>
2017-09-20 07:19:44 +0300

    Added two constants necessary for my firebase database, still on practice, I'm currently using shared preferences

--- app/src/main/java/com/example/wasike/myrestaurants/Constants.java
+++ app/src/main/java/com/example/wasike/myrestaurants/Constants.java
@@ -6,5 +6,9 @@ package com.example.wasike.myrestaurants;
 
 public class Constants {
     public static final String YELP_TOKEN = BuildConfig.YELP_TOKEN;
-
+    public static final String YELP_BASE_URL = "https://api.yelp.com/v3/businesses/search?term=restaurant";
+    public static final String YELP_LOCATION_QUERY_PARAMETER = "location";
+    public static final String PREFERENCES_LOCATION_KEY = "location";
+    public static final String FIREBASE_CHILD_SEARCHED_LOCATION = "searchedLocation";
+    public static final String FIREBASE_CHILD_RESTAURANTS = "restaurants";
 }
