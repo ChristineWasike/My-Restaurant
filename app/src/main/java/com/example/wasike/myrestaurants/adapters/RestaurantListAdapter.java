a4162cf339f387840ddc1d5cf825cf3012129025
Christine Wasike <christinewasike22@gmail.com>
2017-09-20 07:30:36 +0300

    Implemented shared preferences to enable viewing restaurants who's inital zip code was keyed in

--- app/src/main/java/com/example/wasike/myrestaurants/adapters/RestaurantListAdapter.java
+++ app/src/main/java/com/example/wasike/myrestaurants/adapters/RestaurantListAdapter.java
@@ -1,8 +1,91 @@
 package com.example.wasike.myrestaurants.adapters;
 
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.wasike.myrestaurants.R;
+import com.example.wasike.myrestaurants.models.Restaurant;
+import com.example.wasike.myrestaurants.ui.RestaurantDetailActivity;
+import com.squareup.picasso.Picasso;
+
+import org.parceler.Parcels;
+
+import java.util.ArrayList;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 /**
  * Created by wasike on 11/09/17.
  */
 
-public class RestaurantListAdapter {
+public class RestaurantListAdapter extends RecyclerView.Adapter<RestaurantListAdapter.RestaurantViewHolder> {
+    private ArrayList<Restaurant> mRestaurants = new ArrayList<>();
+    private Context mContext;
+    private static final int MAX_WIDTH = 400;
+    private static final int MAX_HEIGHT = 300;
+
+    public RestaurantListAdapter(Context context, ArrayList<Restaurant> restaurants) {
+        mContext = context;
+        mRestaurants = restaurants;
+    }
+
+    @Override
+    public RestaurantListAdapter.RestaurantViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.restaurant_list_item, parent, false);
+        RestaurantViewHolder viewHolder = new RestaurantViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(RestaurantListAdapter.RestaurantViewHolder holder, int position) {
+        holder.bindRestaurant(mRestaurants.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mRestaurants.size();
+    }
+
+    public class RestaurantViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        @Bind(R.id.restaurantImageView) ImageView mRestaurantImageView;
+        @Bind(R.id.restaurantNameTextView) TextView mNameTextView;
+        //@Bind(R.id.categoryTextView) TextView mCategoryTextView;
+        @Bind(R.id.ratingTextView) TextView mRatingTextView;
+
+        private Context mContext;
+
+
+        public RestaurantViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+            mContext = itemView.getContext();
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            int itemPosition = getLayoutPosition();
+            Intent intent = new Intent(mContext, RestaurantDetailActivity.class);
+            intent.putExtra("position", itemPosition);
+            intent.putExtra("restaurants", Parcels.wrap(mRestaurants));
+            mContext.startActivity(intent);
+        }
+
+        public void bindRestaurant(Restaurant restaurant) {
+            Picasso.with(mContext).load(restaurant.getImageUrl()).resize(MAX_WIDTH, MAX_HEIGHT)
+                    .centerCrop().into(mRestaurantImageView);
+            mNameTextView.setText(restaurant.getName());
+            //mCategoryTextView.setText(restaurant.getCategories().get(0));
+            mRatingTextView.setText("Rating: " + restaurant.getRating() + "/5");
+        }
+    }
+
+
 }
