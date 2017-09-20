d99a8acd32a11861e0d95123b95f5ba0df816835
Christine Wasike <christinewasike22@gmail.com>
2017-09-20 07:32:28 +0300

    Added functionality to the save restaurant button to enable a user to save a restaurant

--- app/src/main/java/com/example/wasike/myrestaurants/adapters/RestaurantPagerAdapter.java
+++ app/src/main/java/com/example/wasike/myrestaurants/adapters/RestaurantPagerAdapter.java
@@ -1,8 +1,39 @@
 package com.example.wasike.myrestaurants.adapters;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import com.example.wasike.myrestaurants.models.Restaurant;
+import com.example.wasike.myrestaurants.ui.BlankFragment;
+
+import java.util.ArrayList;
+
 /**
  * Created by wasike on 12/09/17.
  */
 
-public class RestaurantPagerAdapter {
+
+public class RestaurantPagerAdapter extends FragmentPagerAdapter {
+    private ArrayList<Restaurant> mRestaurants;
+
+    public RestaurantPagerAdapter(FragmentManager fm, ArrayList<Restaurant> restaurants) {
+        super(fm);
+        mRestaurants = restaurants;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return BlankFragment.newInstance(mRestaurants.get(position));
+    }
+
+    @Override
+    public int getCount() {
+        return mRestaurants.size();
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mRestaurants.get(position).getName();
+    }
 }
